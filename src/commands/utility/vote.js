301d5a881d95e29f9aa784bc27b50db9c087779f
Glitch (judicious-abiding-church) <none>
2020-09-14 00:11:46 +0000

    🚏🔋 Checkpoint ./src/commands/utility/vote.js:16508698/506

--- src/commands/utility/vote.js
+++ src/commands/utility/vote.js
@@ -11,7 +11,7 @@
  */
 exports.run = async (client, message, args) => {
   client.embed.send(message, { desc: 'I\'ve sent you a private message with the bot invite link.' })
-  message.author.send(`<https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=27681>`)
+
 }
 
 /** Command Config */
